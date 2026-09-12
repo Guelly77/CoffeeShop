85e878da16f1ad06153a592faf2b40bdd29ea517
Guelly77 <migmigspogi@gmail.com>
2026-09-12 23:53:04 +0800

    Change title of the menu.

--- resources/views/livewire/shop/product-manager.blade.php
+++ resources/views/livewire/shop/product-manager.blade.php
@@ -29,7 +29,7 @@
                 <div class="lg:col-span-2 rounded-xl bg-white p-5 shadow">
 
                     <h2 class="mb-4 text-xl font-semibold">
-                        Menu Items
+                        Items Available on the Menu
                     </h2>
 
                     <div class="space-y-3">
